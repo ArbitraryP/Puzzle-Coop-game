bbb370dfb844dfbef63168769c2f824f1ab78aea
ArbitraryP <57426973+ArbitraryP@users.noreply.github.com>
2021-03-08 21:24:56 +0800

    Remade Project
    - Added Mirror Network Package
    - Added Mirror FizzySteamworks Package
    
    Todo
    - Try out the Connecting Invite System

---
+++ Puzzle Coop/Assets/Scripts/Prototype/SteamTest.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Steamworks;
+using UnityEngine;
+
+public class SteamTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!SteamManager.Initialized) { return; }
+
+        string name = SteamFriends.GetPersonaName();
+        Debug.Log("The game is connected to Steam: " + name);
+    }
+}
