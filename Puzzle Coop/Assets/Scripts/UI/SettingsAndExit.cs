d729aef555c8aa9f20a52b7de11a5eaa19275904
ArbitraryP <57426973+ArbitraryP@users.noreply.github.com>
2021-07-05 19:44:35 +0800

    Changed Fullscreen Settings and UI elements
    - Edited Scaling of Main Menu UI elements
    - Made UI_MainMenu and UI_SettingsExit prefabs
    - Added Fullscreen Toggle mode
    - Now Toggles Screen to Smaller when windowed mode
    - Remembers last set Fullscreen mode for the game
    - Change Exit/Quit Button to Toggle
    - Added Exit/Quit Confirm Panel
    
    Todo next:
    
    - 3 options for exit game/lobby/cancel
    
    - Study how to import names from Steam
    - Implement Better Main Menu Navigation
    
    - Plan out Level/Tutorial/Games flow
    - Save Progress System (Host-based/Partner-based)
    - plan out Player prefs of settings

--- Puzzle Coop/Assets/Scripts/UI/SettingsAndExit.cs
+++ Puzzle Coop/Assets/Scripts/UI/SettingsAndExit.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsAndExit : MonoBehaviour
 {
-    
+    [SerializeField] private Toggle toggleFullScreen = null;
+
+    public void Start()
+    {
+        //Set the toggle based on if it is Fullscreen or not
+        toggleFullScreen.isOn = Screen.fullScreen;
+    }
 
     public void SetFullScreen(bool isFullScreen)
     {
-        Screen.fullScreen = isFullScreen;
+        //Screen.fullScreen = isFullScreen;
+
+        if (isFullScreen)
+        {
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+        
+        }
+        else
+        {
+            Screen.SetResolution(1024, 576, false);
+            
+        }
+
+        
+
+
+
     }
 }
